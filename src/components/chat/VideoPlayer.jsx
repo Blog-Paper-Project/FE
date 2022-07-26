b683ec393408465497aa40cd523426ce31ad7513
daegyu-jeong <fade8667@naver.com>
2022-07-26 23:50:56 +0900

    .

--- src/components/chat/VideoPlayer.jsx
+++ src/components/chat/VideoPlayer.jsx
@@ -26,32 +26,22 @@ const useStyles = makeStyles((theme) => ({
 
 const VideoPlayer = () => {
   const {
-    nickname,
     callAccepted,
     myVideo,
     userVideo,
     callEnded,
     stream,
-    call,
   } = useContext(SocketContext);
   const classes = useStyles();
   return (
     <Grid container className={classes.gridContainer}>
       {callAccepted && stream ? (
         <Paper className={classes.paper}>
-          <Grid item xs={12} md={6}>
-            <Typography variant="h5" gutterBottom>
-              Me
-            </Typography>
-            <video
-              playsInline
-              muted
-              ref={myVideo}
-              autoPlay
-              className={classes.video}
-            />
-          </Grid>
-        </Paper>
+        <Grid item xs={12} md={6}>
+          <Typography variant="h5" gutterBottom>Me</Typography>
+          <video playsInline muted ref={myVideo} autoPlay className={classes.video} />
+        </Grid>
+      </Paper>
       ) : (
         <Paper className={classes.paper}>
           <Grid item xs={12} md={6}>
