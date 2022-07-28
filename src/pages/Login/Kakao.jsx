520041aaff322aeb20eda464662d3502b278254f
daegyu-jeong <fade8667@naver.com>
2022-07-28 20:36:34 +0900

    .

--- src/pages/Login/Kakao.jsx
+++ src/pages/Login/Kakao.jsx
@@ -17,12 +17,7 @@ const Kakao = () => {
           .then((data) => {
             console.log(data)
             if (data.data.blogId === null || data.data.blogId === undefined) {
-              setCookie("token", data.data.token, 2);
-              setCookie("nickname", data.data.nickname, 2);
-              setCookie("userId", data.data.userId, 2);
-              setCookie("profileimage", data.data.profileImage, 2);
               setCookie("email", data.data.email, 2);
-
               navigate("/socialsignup");
             } else if (
               data.data.blogId !== null &&
